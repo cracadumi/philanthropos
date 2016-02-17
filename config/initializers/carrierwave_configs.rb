483b15c589a246618ad575fddadb0b313f3b6e56
Antoine Berton <antoine@angeltech.io>
2016-02-17 22:49:43 +0000

    fixing aws upload

--- config/initializers/carrierwave_configs.rb
+++ config/initializers/carrierwave_configs.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: AWS_KEY_ID,
-      aws_secret_access_key: AWS_KEY,
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       url: ":s3_domain_url",
-      region: S3_REGION 
+      region: ENV["AWS_REGION"] 
   }
-  config.fog_directory = BUCKET_NAME
+  config.fog_directory = ENV["S3_BUCKET_NAME"]
   # config.asset_host = ASSET_HOST
 end
