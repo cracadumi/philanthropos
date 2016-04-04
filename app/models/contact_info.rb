2a40be0aa04d48189642dbfe1b12153a569de28d
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-04 16:11:11 +0700

    Use zip code, address and city to geocode.

--- app/models/contact_info.rb
+++ app/models/contact_info.rb
@@ -1,4 +1,5 @@
 class ContactInfo < ActiveRecord::Base
+  geocoded_by :get_user_current_address
 
   validates :adresse, presence: true
   validates :npa_localite, presence: true
@@ -9,42 +10,34 @@ class ContactInfo < ActiveRecord::Base
   # phony_normalize :tel_natel, :default_country_code => 'FR'
   # validates :tel_home, :phony_plausible => true
 
-  geocoded_by :get_user_current_address
   after_validation :geocode
   after_update :geocode
 
   def get_user_current_address
-    # address = [self.npa_localite.scan(/\d+/).map(&:to_i).first, Country[self.pays].name].join(' ')
-    # address
-    # "#{self.current_postcode} #{self.current_town} #{self.current_country}"
-    "#{self.current_address.squish} #{self.current_town} #{self.current_postcode} #{self.current_country}"
+    "#{self.current_postcode} #{self.current_town} #{self.current_country}"
   end
 
   def current_city_and_cp
     "#{self.current_postcode} #{self.current_town}"
-  end  
+  end
 
   def self.translate_pays_to_french
     pays_array = Array.new()
     Country.all.collect do |p|
       c = ISO3166::Country.find_country_by_name(p[0])
-      pays_array << {:code => p[1], :name => c.translations["fr"]}
+      pays_array << { code: p[1], name: c.translations['fr'] }
     end
-    pays_array[7][:name] = "Antilles néerlandaises"
-    
+    pays_array[7][:name] = 'Antilles néerlandaises'
     pays_array
-  end  
+  end
 
   def self.translate_countries_to_french
     pays_array = Array.new()
     Country.all.collect do |p|
       c = ISO3166::Country.find_country_by_name(p[0])
-      pays_array << {:name => p[0], :french_name => c.translations["fr"]}
+      pays_array << { name: p[0], french_name: c.translations['fr'] }
     end
-    pays_array[7][:french_name] = "Antilles néerlandaises"
-
+    pays_array[7][:french_name] = 'Antilles néerlandaises'
     pays_array
-  end  
-
-
+  end
 end
